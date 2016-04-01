d0c80cfa72041c3a31296cabf1779cda26cc8a43
Victor <vkonen1@umbc.edu>
2016-04-01 18:35:43 -0400

    Query for user data and check against google login id;

--- dashboard.php
+++ dashboard.php
@@ -6,5 +6,37 @@ if (!isset($token_data)) {
 	header("Location: index.php");
 }
 
+$google_id = $token_data["sub"];
+
+//mysql server connection info
+$server = "localhost";
+$username = "cmsc447";
+$password = "CMSC447group";
+//connect to the mysql server
+$mysql = mysql_connect($server, $username, $password);
+if (!$mysql) {
+	die("Could not connect to the database: " . mysql_error());
+}
+//select the cmsc447 database
+$db = mysql_select_db("cmsc447", $mysql);
+
+//query for the user data
+$query = "SELECT * FROM Users u WHERE u.GoogleId = " . $google_id;
+$result = mysql_query($query);
+if (!$result) {
+	die("Error: " . mysql_error() . "<br />Query: " . $query);
+}
+
+//kick them out if they don't belong
+$num_results = mysql_num_rows($result);
+if ($num_results != 1) {
+	header("Location: index.php");
+}
+
+//get the user's info from the query
+$user_info = mysql_fetch_assoc($result);
+
+var_dump($user_info);
+
 require("html/dashboard.html.php");
 ?>
