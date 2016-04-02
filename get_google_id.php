0f6b896b46f48717f427d6183a4691e2ecbdc768
Victor <vkonen1@umbc.edu>
2016-04-01 21:35:37 -0400

    Fixing google sign in on get google id script page;

--- get_google_id.php
+++ get_google_id.php
@@ -2,7 +2,7 @@
 require("login.php");
 
 if (isset($authUrl)) { 
-	echo "<a href=\"<?php echo($authUrl); ?>\">Sign-in to Google+</a>";
+	echo "<a href=\"$authUrl\">Sign-in to Google+</a>";
 } else {
 	echo $token_data["payload"]["sub"];
 }
