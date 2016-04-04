86a3756ed0763c1768321b80c4fa1e4335e24c6a
Victor <vkonen1@umbc.edu>
2016-04-03 21:49:55 -0400

    Made logout more secure;

--- logout.php
+++ logout.php
@@ -2,8 +2,17 @@
 //resume the session
 session_start();
 
+//clear the cookie data in the client's browser
+$session_cookie_param = session_get_cookie_params();
+setcookie(session_name(), "", time(),
+	$session_cookie_param['path'], 
+	$session_cookie_param['domain'], 
+	$session_cookie_param['secure'], 
+	$session_cookie_param['httponly']);
+
 //clear session data
 session_unset();
+session_destroy();
 
 //redirect to home
 header("Location: index.php");
