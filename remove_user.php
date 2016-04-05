a82b1f82af1896a2f5cad07e21d897195d6d6131
Victor <vkonen1@umbc.edu>
2016-04-05 13:32:55 -0400

    Implemented remove user; Updated manage user links;

---
+++ remove_user.php
@@ -0,0 +1,66 @@
+<?php
+require("session.php");
+
+//redirect to dashboard if not admin or instructor
+if (!($_SESSION["admin"] || $_SESSION["instructor"])) {
+	header("Location: dashboard.php");
+	exit;
+}
+
+//set the management type
+if (isset($_REQUEST["administrators"])) {
+	$management = "administrators";
+} else if (isset($_REQUEST["instructors"])) {
+	$management = "instructors";
+} else if (isset($_REQUEST["students"])) {
+	$management = "students";
+}
+
+//some type must be added instructors can only remove students
+//user_id must be set
+if (!isset($management) || (!$_SESSION["admin"] && $management != "students") || !isset($_GET["user_id"])) {
+	header("Location: manage_users.php");
+	exit;
+}
+
+//get the user id of the user to remove from the user type table
+$user_id = $_GET["user_id"];
+
+//prevent user from removing themselves
+if ($_SESSION["user_id"] == $user_id) {
+	header("Location: manage_users.php?" . $management);
+	exit;
+}
+
+//mysql server connection info
+$server = "localhost";
+$username = "cmsc447";
+$password = "CMSC447group";
+//connect to the mysql server
+$mysql = mysql_connect($server, $username, $password);
+if (!$mysql) {
+	die("Could not connect to the database: " . mysql_error());
+}
+//select the cmsc447 database
+$db = mysql_select_db("cmsc447", $mysql);
+
+$query = "SELECT * FROM " . ucfirst($management) . " ut WHERE ut.UserId = '" . $user_id . "'";
+$result = mysql_query($query);
+if (!$result) {
+	die("Error: " . mysql_error() . "<br />Query: " . $query);
+}
+
+//remove the user if they exist in the table
+$num_results = mysql_num_rows($result);
+if ($num_results > 0) {
+	$query = "DELETE FROM " . ucfirst($management) . " WHERE UserId = '" . $user_id . "'";
+	$result = mysql_query($query);
+	if (!$result) {
+		die("Error: " . mysql_error() . "<br />Query: " . $query);
+	}
+}
+
+//back to the management page
+header("Location: manage_users.php?" . $management);
+exit;
+?>
